b4d6cfd270d79483b5603fbe3a748fad2e149c67
Allison <allison@example.com>
2021-01-18 21:45:31 -0500

    Verifying held item names

--- cypress/integration/pikachuTest.spec.js
+++ cypress/integration/pikachuTest.spec.js
@@ -1,90 +1,84 @@
-describe('API Testing with Cypress', () => {
+describe("API Testing with Cypress", () => {
   beforeEach(() => {
-    cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pikachu');
-  });
+    cy.request("https://pokeapi.co/api/v2/pokemon/25").as("pikachu")
+  })
 
-  it('Validate the header', () => {
-    cy.get('@pikachu')
-      .its('headers')
-      .its('content-type')
-      .should('include', 'application/json; charset=utf-8');
-  });
+  it("Validate the header", () => {
+    cy.get("@pikachu")
+      .its("headers")
+      .its("content-type")
+      .should("include", "application/json; charset=utf-8")
+  })
 
-  it('Validate the status code', () => {
-    cy.get('@pikachu')
-      .its('status')
-      .should('equal', 200);
-  });
+  it("Validate the status code", () => {
+    cy.get("@pikachu")
+      .its("status")
+      .should("equal", 200)
+  })
 
-  it('Validate the pokemon\'s name', () => {
-    cy.get('@pikachu')
-      .its('body')
-      .should('include', { name: 'pikachu' });
-  });
+  it("Validate the pokemon's name", () => {
+    cy.get("@pikachu")
+      .its("body")
+      .should("include", { name: "pikachu" })
+  })
 
-  it('checks the height', ()=> {
-    cy.get('@pikachu')
-      .its('body')
-      .should('include', { height: 4 });
-  });
+  it("checks the height", () => {
+    cy.get("@pikachu")
+      .its("body")
+      .should("include", { height: 4 })
+  })
 
-  it('checks the id', ()=> {
-    cy.get('@pikachu')
-      .its('body')
-      .should('include', { id: 25 });
-  });
+  it("checks the id", () => {
+    cy.get("@pikachu")
+      .its("body")
+      .should("include", { id: 25 })
+  })
 
-  it('checks the moves length', ()=> {
-    cy.get('@pikachu')
-      .its('body.moves.length')
-      .should('equal', 81)
-  });
+  it("checks the moves length", () => {
+    cy.get("@pikachu")
+      .its("body.moves.length")
+      .should("equal", 81)
+  })
 
-  it('validate first move name', ()=> {
-    cy.get('@pikachu')
-      .its('body.moves.0.move.name')
-      .should('equal', 'mega-punch')
-  });
+  it("validate first move name", () => {
+    cy.get("@pikachu")
+      .its("body.moves.0.move.name")
+      .should("equal", "mega-punch")
+  })
 
-  it('validatesfirst move url', ()=> {
-    cy.get('@pikachu')
-      .its('body.moves.0.move.url')
-      .should('equal','https://pokeapi.co/api/v2/move/5/')
-  });
+  it("validates first move url", () => {
+    cy.get("@pikachu")
+      .its("body.moves.0.move.url")
+      .should("equal", "https://pokeapi.co/api/v2/move/5/")
+  })
 
-  it('validates second move name',()=> {
-    cy.get('@pikachu')
-      .its('body.moves.1.move.name')
-      .should('equal', 'pay-day')
-  });
+  it("validates last move name", () => {
+    cy.get("@pikachu")
+      .its("body.moves.80.move.name")
+      .should("equal", "nuzzle")
+  })
 
-  it('validates the second move url', ()=> {
-    cy.get('@pikachu')
-      .its('body.moves.1.move.url')
-      .should('equal','https://pokeapi.co/api/v2/move/6/')
-  });
+  it("validates the last move url", () => {
+    cy.get("@pikachu")
+      .its("body.moves.80.move.url")
+      .should("equal", "https://pokeapi.co/api/v2/move/609/")
+  })
 
-  it('validates third move name', ()=> {
-    cy.get('@pikachu')
-      .its('body.moves.2.move.name')
-      .should('equal', 'thunder-punch')
-  });
+  it("validates the held items length", () => {
+    cy.get("@pikachu")
+      .its("body.held_items.length")
+      .should("equal", 2)
+  })
 
-  it('validates third move url', () => {
-    cy.get('@pikachu')
-      .its('body.moves.2.move.url')
-      .should('equal','https://pokeapi.co/api/v2/move/9/')
-  });
+  it("validates the name of the first item among the held items", () => {
+    cy.get("@pikachu")
+      .its("body.held_items.0.item.name")
+      .should("equal", "oran-berry")
+  })
 
-  it('validates last move name', ()=> {
-    cy.get('@pikachu')
-      .its('body.moves.80.move.name')
-      .should('equal', 'nuzzle')
-  });
-
-  it('validates last move url', ()=> {
-    cy.get('@pikachu')
-      .its('body.moves.80.move.url')
-      .should('equal', 'https://pokeapi.co/api/v2/move/609/')
-  });
-});
+  it("validates the name of the second item among the held items ", () => {
+    cy.get("@pikachu")
+      .its("body.held_items.1.item.name")
+      .should("equal", "light-ball")
+  })
+})
